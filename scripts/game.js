8eca693a0ae59e1e71a5a928878c38d4143b77f6
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2025-02-05 13:53:35 +0200

    added images

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,33 @@
-function restGameStatus() {
+function resetGameStatus() {
   activePlayer = 0;
   currentRound = 1;
-  gameIsOver=false;
+  gameIsOver = false;
   gameOverElement.firstElementChild.innerHTML =
-    'You Won <span id="winner-name">Player Name</span>!';
-  gameOverElement.style.display = "none";
+    'You won, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = 'none';
+
   let gameBoardIndex = 0;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       gameData[i][j] = 0;
-      const gameBoardItem = gameBoardElement.children[gameBoardIndex];
-      gameBoardItem.textContent = "";
-      gameBoardItem.classList.remove("disabled");
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = '';
+      gameBoardItemElement.classList.remove('disabled');
       gameBoardIndex++;
     }
   }
 }
+
 function startNewGame() {
-  if (players[0] === "" || players[1] === "") {
-    alert("Please set custom player names for both players");
+  if (players[0].name === '' || players[1].name === '') {
+    alert('Please set custom player names for both players!');
     return;
   }
-  restGameStatus();
-  activePlayerNameElement.textContent = players[activePlayer];
 
-  gameAreaElement.style.display = "block";
+  resetGameStatus();
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = 'block';
 }
 
 function switchPlayer() {
@@ -33,35 +36,40 @@ function switchPlayer() {
   } else {
     activePlayer = 0;
   }
-  activePlayerNameElement.textContent = players[activePlayer];
+  activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
 function selectGameField(event) {
-  if (event.taget.tagName !== "LI"||gameIsOver) {
+  if (event.target.tagName !== 'LI' || gameIsOver) {
     return;
   }
+
   const selectedField = event.target;
-
   const selectedColumn = selectedField.dataset.col - 1;
-  const selecetedRow = selectedField.dataset.row - 1;
+  const selectedRow = selectedField.dataset.row - 1;
 
-  if (gameData[selecetedRow][selectedColumn] > 0) {
-    alert("Please select an empty field!");
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert('Please select an empty field!');
     return;
   }
 
   selectedField.textContent = players[activePlayer].symbol;
-  selectedField.classList.add("disabled");
+  selectedField.classList.add('disabled');
 
-  gameData[selecetedRow][selectedColumn] = activePlayer + 1;
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
   const winnerId = checkForGameOver();
+
   if (winnerId !== 0) {
     endGame(winnerId);
   }
+
   currentRound++;
   switchPlayer();
 }
+
 function checkForGameOver() {
+  // Checking the rows for equality
   for (let i = 0; i < 3; i++) {
     if (
       gameData[i][0] > 0 &&
@@ -72,15 +80,17 @@ function checkForGameOver() {
     }
   }
 
+  // Checking the columns for equality
   for (let i = 0; i < 3; i++) {
     if (
       gameData[0][i] > 0 &&
       gameData[0][i] === gameData[1][i] &&
       gameData[0][i] === gameData[2][i]
     ) {
-      return gameData[0][0];
+      return gameData[0][i];
     }
   }
+
   // Diagonal: Top left to bottom right
   if (
     gameData[0][0] > 0 &&
@@ -89,7 +99,8 @@ function checkForGameOver() {
   ) {
     return gameData[0][0];
   }
-  //Diagonal: Bottom left to top right
+
+  // Diagonal: Bottom left to top right
   if (
     gameData[2][0] > 0 &&
     gameData[2][0] === gameData[1][1] &&
@@ -97,14 +108,18 @@ function checkForGameOver() {
   ) {
     return gameData[2][0];
   }
+
   if (currentRound === 9) {
     return -1;
   }
+
   return 0;
 }
+
 function endGame(winnerId) {
-    gameIsOver=true;
-  gameOverElement.style.display = "block";
+  gameIsOver = true;
+  gameOverElement.style.display = 'block';
+
   if (winnerId > 0) {
     const winnerName = players[winnerId - 1].name;
     gameOverElement.firstElementChild.firstElementChild.textContent =
